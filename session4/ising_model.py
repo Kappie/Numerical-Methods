c5a0b260b242ccdf3b2f6f196e3aaa0a46e9dca5
Kappie <ghkapteijns@gmail.com>
2016-02-11 14:26:03 +0000

    shit is going awry with global variables. Switching to object oriented.

--- session4/ising_model.py
+++ session4/ising_model.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plot
 import itertools
 import pdb
 
-T = 3   # Temperature
-J = 1   # Coupling constant
-L = 4   # System size
+temperatures = np.array([0.125, 0.25, 0.5, 1, 2, 4])
+L = 4
 N = L * L
+J = 1   # Coupling constant
 
 n_thermalization = 10000
 n_sweeps = 2 ** 14
@@ -76,7 +76,45 @@ def take_steps(config, n):
 
     return config
 
+def binning_analysis(samples):
+    """
+    Assumes samples.size is a power of 2. Returns an array with the naive monte carlo error estimate sigma / sqrt(N - 1)
+    (assumption that samples are uncorrelated) for each binning order, starting with order 0 (i.e.
+    all samples in their own bin) down to the minimum number of bins.
+    """
+    minimum_number_of_bins = 2 ** 6
+    binning_steps = int( np.log2(samples.size / minimum_number_of_bins) ) + 1
+    bins = np.copy(samples)
 
+    errors = np.empty(binning_steps)
+    for l in range(binning_steps):
+        errors[l] = np.std(bins) / np.sqrt(bins.size - 1)
+        # Take average of consecutive bins for next binning order
+        bins = np.array( (bins[::2] + bins[1::2]) / 2 )
+
+    return errors
+
+def convergence_rate(errors):
+    """
+    Determines the average of the last few relative changes of an array of error estimates of a binning analysis.
+    """
+    relative_changes = errors[1:] - errors[:-1] / errors[1:]
+    return np.mean(relative_changes[-3:])
+
+def integrated_autocorrelation_time(bin_errors):
+    return 0.5 * ( (errors[-1] / errors[0]) ** 2 - 1 )
+
+def measurement(observable, temperatures, initial_config):
+    measurements = np.empty(temperatures.size)
+
+    for temp in temperatures:
+        samples = np.empty(n_sweeps)
+
+        for i in range(n_sweeps):
+            config = take_steps(config, N)
+            samples[i] = observable(config)
+
+        return measurements
 
 ### MAIN LOOP ###
 # Thermalize the markov chain
@@ -84,19 +122,32 @@ config = random_configuration()
 config = take_steps(config, n_thermalization)
 
 # Perform measurements
-measurements_order_parameter = np.empty(n_sweeps)
-measurements_energy_per_site = np.empty(n_sweeps)
-measurements_magnetization_squared = np.empty(n_sweeps)
-measurements_magnetization_per_site = np.empty(n_sweeps)
-for i in range(n_sweeps):
-    # Measure after performing N = L * L metropolis steps
-    config = take_steps(config, N)
-    measurements_order_parameter[i] = order_parameter(config)
-    measurements_energy_per_site[i] = energy_per_site(config)
-    measurements_magnetization_squared[i] = magnetization_squared(config)
-    measurements_magnetization_per_site[i] = magnetization_per_site(config)
+order_parameters = np.empty(temperatures.size)
+# for T in temperatures:
+#     measurements = np.empty(n_sweeps)
+#
+#     for i in range(n_sweeps):
+#         # Measure after performing N = L * L metropolis steps
+#         config = take_steps(config, N)
+#         measurements[i] = order_parameter(config)
+#         # measurements_energy_per_site[i] = energy_per_site(config)
+#         # measurements_magnetization_squared[i] = magnetization_squared(config)
+#         # measurements_magnetization_per_site[i] = magnetization_per_site(config)
+#
+#     bin_errors = binning_analysis(measurements)
+#     plot.plot(bin_errors, 'ko')
+#     plot.xlabel("binning step")
+#     plot.ylabel("estimated error")
+#     print "<m>: {m} +/- {error}.".format(m = np.mean(measurements), error = bin_errors[-1])
+#     if convergence_rate(bin_errors) > 0.05:
+#         print """Warning: binning analysis did not converge on error estimate.
+#         Mean relative change of last few estimates was only {change}.""".format(change = convergence_rate(bin_errors))
+#
+# plot.show()
 
-print "<E/N>", np.mean(measurements_energy_per_site)
-print "<m>  ", np.mean(measurements_order_parameter)
-print "<M^2>", np.mean(measurements_magnetization_squared)
-print "<M/N>", np.mean(measurements_magnetization_per_site)
+plot.plot([(1, 0), (2, 1), (3, 10), (4, 20)])
+plot.show()
+
+# print "<E/N>", np.mean(measurements_energy_per_site)
+# print "<M^2>", np.mean(measurements_magnetization_squared)
+# print "<M/N>", np.mean(measurements_magnetization_per_site)
